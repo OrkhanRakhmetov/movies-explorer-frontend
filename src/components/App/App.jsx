80cf260e422dea9a97ce4b829ea278d74e256555
OrkhanRakhmetov <orakhmetov@mail.ru>
2023-07-07 19:50:55 +0300

    создал компоненты и файловая структура

---
+++ src/components/App/App.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from 'react';
+import { Routes, Route, useNavigate} from 'react-router-dom';
+
+import CurrentUserContext from "../../contexts/CurrentUserContext";
+
+import './App.css';
+
+function App() {
+
+
+  return (
+    <CurrentUserContext.Provider>
+     
+    </CurrentUserContext.Provider>
+  );
+}
+
+export default App;
